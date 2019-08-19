2ab68635afd362538bb3a1cd3b02c8b6c2ee17c3
Matt Viteri <mxviteri@gmail.com>
2019-08-19 16:26:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## !! INSTRUCTOR REPO !!
+
+This project should be complete and fully executable
+
+## Setup
+
+Clone this repo and run `npm i` and then `npm start`
+
+## Project Instructions
+
+* We are adding a login page to the FakeCars.com application. Once complete, you will be able to login to the app and you will remain logged in on page refresh until the cookie expires at one mintes time.
---
+++ src/Router.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+import cookie from 'cookie'
+import Home from './components/Home'
+import About from './components/About'
+import Car from './components/Car'
+import Login from './components/Login'
+
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route
+        {...rest}
+        render={(props) => checkAuth() 
+            ? <Component {...props} />
+            : <Redirect to="/login" />}
+        />
+    )
+}
+
+const Router = () => {
+    return (
+        <Switch>
+            <Route path="/login" component={Login} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute path="/about" component={About} />
+            <ProtectedRoute path="/car/:id" component={Car} />
+        </Switch>
+    );
+};
+
+export default Router;
